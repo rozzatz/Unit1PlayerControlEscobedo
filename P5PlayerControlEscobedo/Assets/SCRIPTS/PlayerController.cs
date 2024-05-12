58680b732d36d27bc52186acb703221f1cb9d1b9
bruce <753825@student.dusd.net>
2024-05-12 14:39:32 -0700

    ;k

--- P5PlayerControlEscobedo/Assets/SCRIPTS/PlayerController.cs
+++ P5PlayerControlEscobedo/Assets/SCRIPTS/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
     void Start()
     {
         
@@ -22,6 +26,12 @@ public class PlayerController : MonoBehaviour
     
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up,  turnSpeed *horizontalInput * Time.deltaTime );
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
 
     }
 }
